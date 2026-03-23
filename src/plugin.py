76ea9c75ccac18142ab644b386da6b7c65a7de21
qvvonk <qvvonk@gmail.com>
2026-03-23 15:00:20 +0300

    properties

--- src/plugin.py
+++ src/plugin.py
@@ -1,6 +1,14 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
 from funpayhub.app.plugin import Plugin
+from .properties import DemperProperties
+
+if TYPE_CHECKING:
+    from funpayhub.lib.properties import Properties
 
 
 class DempingPlugin(Plugin):
-    ...
-
+    async def properties(self) -> Properties:
+        props = DemperProperties()
+        return props
